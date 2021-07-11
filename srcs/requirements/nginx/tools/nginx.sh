f69a1c9004711e86ee51a40595c96c5cd93707e5
Yazid Ihssan <yihssan@e0r2p4.1337.ma>
2021-07-11 20:52:05 +0100

    ada

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,10 @@
+chown -R www-data /var/www/*
+chmod -R 755 /var/www/*
+
+
+mkdir /var/www/yihssan && touch /var/www/yihssan/index.php
+echo "<?php phpinfo(); ?>" >> /var/www/yihssan/index.php
+
+mv ./yihssan /etc/nginx/sites-available/yihssan
+ln -s /etc/nginx/sites-available/yihssan /etc/nginx/sites-enabled/yihssan
+rm -rf /etc/nginx/sites-enabled/default
