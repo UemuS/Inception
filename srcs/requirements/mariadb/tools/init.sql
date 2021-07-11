44bf428c6c3f2dd51b46a841369dc1e74a532458
UemuS <ak-u-ma@outlook.fr>
2021-07-11 08:38:58 +0100

    Ada

--- srcs/requirements/mariadb/tools/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'yihssan'@'%' IDENTIFIED BY 'yihssan';
-GRANT ALL PRIVILEGES ON *.* TO 'yihssan'@'%';
-FLUSH PRIVILEGES;
-update mysql.user set plugin = 'mysql_native_password' where user='root';
