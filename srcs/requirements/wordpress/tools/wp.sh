9976e9ddb0c2ca3456799abb58a071551ac248b9
Yazid Ihssan <yihssan@e2r13p5.1337.ma>
2021-09-06 12:02:04 +0100

    ada

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,10 +1,9 @@
-mkdir -p /var/www/html
-chown www-data:www-data /var/www/*
-chmod -R 755 /var/www/*
+chown -R www-data /var/www/html
+chmod -R 755 /var/www/html/
 echo "<?php phpinfo(); ?>" >> /var/www/html/index.php
 wget -c https://wordpress.org/latest.tar.gz
 tar -xvzf latest.tar.gz
-mv wordpress/* /var/www/html
+mv wordpress/* /var/www/html/
 sed -i "s/CHANGE_DB_HOST/$WP_HOSTNAME/g" /wp-config.php
 sed -i "s/CHANGE_DB_NAME/$WP_DATABASE_NAME/g" /wp-config.php
 sed -i "s/CHANGE_DB_USER/$WP_DATABASE_USER/g" /wp-config.php
