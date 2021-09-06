02673acac21352860be14538e70a7612f317fd0a
Yazid Ihssan <yihssan@e2r13p5.1337.ma>
2021-09-06 10:46:18 +0100

    meh

--- newmachine.sh
+++ newmachine.sh
@@ -1,4 +1,5 @@
 apt update
 apt install curl make docker
+apt install docker.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
